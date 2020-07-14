058d3d0ef0e68d386127a3ec834dbea319ffe353
MicroOptimization <jackiezhen538@gmail.com>
2020-07-14 18:21:37 -0400

    you can now specific which barber profile you want to look at in the url

--- routes/barberProfile.js
+++ routes/barberProfile.js
@@ -6,16 +6,29 @@ let getUserService = require("../services/getUserService");
 let express = require("express");
 let router = express.Router();
 
-router.get("/", async (req, res, next) => {
-    barberId = "bjackie" //Temp, for testing purposes
+router.get("/:profileId", async (req, res, next) => {
+    barberId = req.params.profileId
+
+    let url = req.originalUrl;
+    console.log(req.params.profileId)
+    console.log(barberId)
+
+    name = "Name not Found"
+    bio = "Bio not Found"
+    contactInfo = "Contact info not found"
+    ratingNum = "Rating not found"
+    
     let barber = await getUserService.getUser(barberId);
     
-    name = barber.username
-    bio = barber.bio
-    contactInfo = barber.contactInfo
-    ratingNum = barber.rating
-
+    if (barber) {
+        name = barber.username
+        bio = barber.bio
+        contactInfo = barber.contactInfo
+        ratingNum = barber.rating
+        
+    }
     res.render("barberProfile", {title: name, about: bio, contact: contactInfo, rating: ratingNum});
 });
+
 
 module.exports = router;
