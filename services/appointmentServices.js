7c834aa8eeae9d6786c378051f9050ce8f84a012
Julian <jchumtl@gmail.com>
2020-07-17 14:32:07 -0400

    Continuing appointment setting

--- services/appointmentServices.js
+++ services/appointmentServices.js
@@ -4,16 +4,26 @@ let getUserId = require("./profileServices").getUserId;
 let Appointment = require("../models/Appointments").Appointment;
 
 let createAppointment = async (startTime, endTime, profileId, customerId) => {
-	let barberId = await getUserId(profileId);
-	let appointment = new Appointment({
-		startTime: startTime,
-		endTime: endTime,
-		barber: barberId,
-		customer: customerId
-	});
+	let response = "";
+	// Validate against front-end script modifications (and changing to unavailable times)
+	let starTimeValidator = (startTime.getMinutes() % 30 == 0);
+	let endTimeValidator = (endTime.getMinutes() === startTime.getMinutes() + 30);
 
-	// Test error/response to saving document
-	await appointment.save();
+	if (starTimeValidator && endTimeValidator) {
+		let barberId = await getUserId(profileId);
+		let appointment = new Appointment({
+			startTime: startTime,
+			endTime: endTime,
+			barber: barberId,
+			customer: customerId
+		});
+
+		// Test error/response to saving document
+		await appointment.save();
+	} else {
+		response = "Appointment not created"
+	}
+	return response;
 }
 
 module.exports = {createAppointment};
